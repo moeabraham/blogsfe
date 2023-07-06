b4867161fece7cbed615bc7a44e2b9eed5e4d80b
Mohamed Soliman <60075091+moeabraham@users.noreply.github.com>
2023-07-06 14:48:26 -0500

    fixes cors

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 
 
-const BASEURL= "https://blogsbe-de5369cd7cdb.herokuapp.com"
+// const BASEURL= "https://blogsbe-de5369cd7cdb.herokuapp.com"
+const BASEURL= "http://localhost:4000"
 
 
 export default BASEURL;
