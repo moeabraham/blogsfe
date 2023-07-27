87ad839541587485b3b64cd47d27b156e0e1f7e4
Mohamed Soliman <60075091+moeabraham@users.noreply.github.com>
2023-07-27 15:41:59 -0500

    fixes cors issues

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -30,13 +30,14 @@ export default function CreatePost(){
             mode:"cors",
             body: data,
             credentials: 'include',
+            // Access-Control-Allow-Origin
+            
+            headers: {
+                // "Content-Type": "application/json",
 
-        //     headers: {
-        //         // "Content-Type": "application/json",
-
-        //         // "Content-Type": "application/json",
-        //         'Access-Control-Allow-Origin': "https://blogsfe.netlify.app/"
-        // },
+                // "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': "https://blogsfe.netlify.app/"
+        },
 
             // mode:"no-cors",
 
