7d49eb7fd3237c8afffcbf54102d1c7dbedbc965
Mohamed Soliman <60075091+moeabraham@users.noreply.github.com>
2023-08-14 17:50:49 -0500

    create post

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -31,12 +31,12 @@ export default function CreatePost(){
             body: data,
             credentials: 'include',
             // Access-Control-Allow-Origin
-        //     headers: {
+            headers: {
         //         // "Content-Type": "application/json",
 
         //         // "Content-Type": "application/json",
-        //         'Access-Control-Allow-Origin': "https://blogsfe.netlify.app/"
-        // },
+                'Access-Control-Allow-Origin': "https://blogsfe.netlify.app/"
+        },
 
             // mode:"no-cors",
 
